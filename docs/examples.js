adf9a772db56f080449a48f0c9416b7527b55898
Alex Sandiiarov <sandiyarov@gmail.com>
2018-11-02 18:49:40 -0500

    docs: ✏️ upd docs

---
+++ docs/examples.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  useActive,
+  useFocus,
+  useHover,
+  useTouch,
+} from '../dist/use-events.esm';
+
+export const UseActiveExample = () => {
+  const [isActive, bind] = useActive();
+
+  return (
+    <div {...bind}>
+      You are {isActive ? 'clicking' : 'not clicking'} this div
+    </div>
+  );
+};
+
+export const UseFocusExample = () => {
+  const [isFocused, bind] = useFocus();
+
+  return (
+    <div>
+      <input {...bind} placeholder="Focus me" />
+      <div>You are {isFocused ? 'focusing' : 'not focusing'} the input</div>
+    </div>
+  );
+};
+
+export const UseHoverExample = () => {
+  const [isHovered, bind] = useHover();
+
+  return (
+    <div {...bind}>
+      You are {isHovered ? 'hovering' : 'not hovering'} this div
+    </div>
+  );
+};
+
+export const UseTouchExample = () => {
+  const [isTouched, bind] = useTouch();
+
+  return (
+    <div {...bind}>
+      You are {isTouched ? 'touching' : 'not touching'} this div
+    </div>
+  );
+};
