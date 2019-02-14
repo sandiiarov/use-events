33e7a35185a7ebbb5bb8853d5c2f6374dec2c74a
Alex Sandiiarov <sandiyarov@gmail.com>
2019-02-13 22:54:14 -0600

    ci: 🎡 fix building process

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { default as useActive } from './useActive';
-export { default as useFocus } from './useFocus';
-export { default as useHover } from './useHover';
-export { default as useTouch } from './useTouch';
-export { default as useClickOutside } from './useClickOutside';
-export { default as useMousePosition } from './useMousePosition';
+export { default as useActive } from './useActive/index';
+export { default as useClickOutside } from './useClickOutside/index';
+export { default as useFocus } from './useFocus/index';
+export { default as useHover } from './useHover/index';
+export { default as useMousePosition } from './useMousePosition/index';
+export { default as useTouch } from './useTouch/index';
