76a669f3e77879995c7a27ed8a9f00570b2c4148
Alex Sandiiarov <sandiyarov@gmail.com>
2019-03-09 21:38:05 -0600

    refactor: 💡 useResizeObserver

--- src/useResizeObserver/index.tsx
+++ src/useResizeObserver/index.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
 
-const useResizeObserver = (ref: React.RefObject<HTMLElement>) => {
-  const [rect, setRect] = React.useState({
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    height: 0,
-    width: 0,
-    x: 0,
-    y: 0,
-  });
+const useResizeObserver = (
+  ref: React.RefObject<HTMLElement>
+): [number, number] => {
+  const [width, setWidth] = React.useState(0);
+  const [height, setHeight] = React.useState(0);
 
   React.useLayoutEffect(() => {
     const { current } = ref;
 
     const resizeObserver = new ResizeObserver(entries => {
-      setRect(entries[0].contentRect);
+      const { width, height } = entries[0].contentRect;
+
+      setWidth(width);
+      setHeight(height);
     });
 
     if (current !== null) {
@@ -29,7 +26,7 @@ const useResizeObserver = (ref: React.RefObject<HTMLElement>) => {
     };
   }, []);
 
-  return rect;
+  return [width, height];
 };
 
 export default useResizeObserver;
