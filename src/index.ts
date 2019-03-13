942642692d99fb941ed9d21c37e93f87cc266b77
William Wang <anderson.thereisnospoon@gmail.com>
2019-03-13 16:52:07 +0800

    Added useScroll

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { default as useFocus } from './useFocus/index';
 export { default as useHover } from './useHover/index';
 export { default as useMousePosition } from './useMousePosition/index';
 export { default as useResizeObserver } from './useResizeObserver/index';
+export { default as useScroll } from './useScroll/index';
 export { default as useTouch } from './useTouch/index';
 export { default as useWindowResize } from './useWindowResize/index';
