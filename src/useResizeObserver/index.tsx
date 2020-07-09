e8fbca06bc3f2180ac52347d912a792e22849224
Alex Sandiiarov <sandiyarov@gmail.com>
2020-07-09 16:17:09 -0400

    Revert "Update useResizeObserver"
    This reverts commit deca24bca015637cbb3f32ab6d21f2adb358ba1f.

--- src/useResizeObserver/index.tsx
+++ src/useResizeObserver/index.tsx
@@ -1,40 +1,23 @@
-import React from "react";
+import React from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
 
 function useResizeObserver(
   ref: React.RefObject<HTMLElement>
 ): [number, number] {
-  const animationFrameID = React.useRef<number>();
   const [width, setWidth] = React.useState(0);
   const [height, setHeight] = React.useState(0);
 
   React.useLayoutEffect(() => {
-    // https://github.com/microsoft/TypeScript/issues/37861
-    // @ts-ignore
-    const resizeObserver = new ResizeObserver((entries) => {
-      if (!Array.isArray(entries) && entries.length !== 0) {
-        return;
-      }
-
-      const { width, height } = entries[0].contentRect;
-
-      // https://github.com/WICG/resize-observer/issues/38
-      animationFrameID.current = requestAnimationFrame(() => {
-        setWidth(width);
-        setHeight(height);
-      });
+    const resizeObserver = new ResizeObserver(entries => {
+      setWidth(entries[0].contentRect.width);
+      setHeight(entries[0].contentRect.height);
     });
 
     if (ref.current !== null) {
       resizeObserver.observe(ref.current);
     }
 
-    return () => {
-      if (animationFrameID.current) {
-        cancelAnimationFrame(animationFrameID.current);
-      }
-
-      resizeObserver.disconnect();
-    };
+    return () => void resizeObserver.disconnect();
   }, [ref]);
 
   return [width, height];
